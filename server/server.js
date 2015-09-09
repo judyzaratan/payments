90f40806b7f050ce36f0e3e555915673ea14559a
mochicat8 <mochicat8@gmail.com>
2015-09-09 04:44:54 -0700

    update port

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+
 var requestHandler = require("./request-handler.js");
 
+var port = process.env.PORT || 8000;
 
 //start up server
-var port = 8000;
 http.createServer(requestHandler.requestHandler).listen(port);
 console.log('Server running at http://127.0.0.1:' + port + '/');
