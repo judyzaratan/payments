0f3ebce2809c37c75d6ddc93cf494a203bb6fead
mochicat8 <mochicat8@gmail.com>
2015-09-08 22:54:30 -0700

    Fixes css & validation, display preloader

--- client/js/functions.js
+++ client/js/functions.js
@@ -12,8 +12,11 @@ var send = getElement("send");
 var transactions = getElement("transactions");
 var recipient = getElement("sendTo");
 var money = getElement("money");
+var next = getElement("next");
 var hideDisplay = function(whichDisplay) {
+    console.log(whichDisplay);
     whichDisplay.style.display = "none";
+
 };
 
 
@@ -23,29 +26,33 @@ var showDisplay = function(input) {
 
 
 recipient.addEventListener("focusin", function() {
-    console.log(this);
     var errors = document.getElementsByClassName("error");
     var error = Array.prototype.forEach.call(errors, function(errorEl) {
         hideDisplay(errorEl);
     });
 });
 
-recipient.addEventListener("focusout", function(){
+//Ask user to update email error after field focusout
+recipient.addEventListener("focusout", function() {
     checkEmail();
-})
+});
 
-//When user focus out of amount input box
-//Display format
-money.addEventListener("focusout", function() {
-    console.log(this);
-    if (this.value !== "" && validateAmount()) {
-        hideDisplay(this);
+document.getElementById("moneyEx").addEventListener("focusin", function() {
+    var errors = document.getElementsByClassName("errorMoney");
+    var error = Array.prototype.forEach.call(errors, function(errorEl) {
+        hideDisplay(errorEl);
+    });
+        document.getElementById("money_format").style.display = "";
+});
+
+document.getElementById("moneyEx").addEventListener("focusout", function() {
+    if (validateAmount()) {
         displayMoney();
     }
 });
 
+
 function displayMoney() {
-    document.getElementById("money").value;
     var options = {
         USD: function() {
             var n = parseFloat(document.getElementById("money").value, 2);
@@ -63,12 +70,16 @@ function displayMoney() {
             var n = Math.round(parseFloat(document.getElementById("money").value));
             console.log(n);
             return n;
-
         }
+    };
+    var sign = {
+        USD: '$',
+        EUR: '&euro;',
+        JPY: '&#165;'
     };
     var selected = document.getElementById("currency").value;
     var money = typeof document.getElementById("money").value;
-    console.log(options[selected]());
+    document.getElementById("money_format").innerHTML = (sign[selected] + options[selected]());
 }
 
 //Add event listener if user changes currency
@@ -87,13 +98,30 @@ viewtransactions.addEventListener("click", function() {
     showDisplay("transactions");
 });
 
+next.addEventListener("click", function(){
+    document.getElementById("preloader").style.display = "inherit";
+    setTimeout(function(){
+        hideDisplay(send);
+        showDisplay("transactions");
+    }, 2000);
+
+});
+document.getElementById("clear").addEventListener("click", function(){
+    document.getElementById("sender").reset();
+    var errors = document.getElementsByClassName("error");
+    Array.prototype.forEach.call(errors, function(errorEl) {
+        hideDisplay(errorEl);
+    });
+    var money_display=document.getElementById("money_format");
+    money_display.innerHTML = "";
+    hideDisplay(document.getElementById("money_format"));
+})
 
 //Form validation
 
 function formValidation() {
     //Assume true
     var isValid = true;
-
 
     var money = getElement("money");
     var payment_cat = getElement("payment_type");
@@ -104,7 +132,6 @@ function formValidation() {
 
 function validateAmount() {
     if (money.value === "") {
-        console.log("errorEmpty");
         showDisplay("errorAmountEmpty");
         return false;
     }
@@ -151,56 +178,51 @@ fetching.addEventListener("click", function() {
 //     }
 // };
 function disableScroll() {
-  if (window.addEventListener) // older FF
-      window.addEventListener('DOMMouseScroll', event.preventDefault, false);
-  window.onwheel = event.preventDefault; // modern standard
-  window.onmousewheel = document.onmousewheel = event.preventDefault; // older browsers, IE
-  window.ontouchmove  = event.preventDefault; // mobile
-  document.onkeydown  = event.preventDefaultForScrollKeys;
+    if (window.addEventListener) // older FF
+        window.addEventListener('DOMMouseScroll', event.preventDefault, false);
+    window.onwheel = event.preventDefault; // modern standard
+    window.onmousewheel = document.onmousewheel = event.preventDefault; // older browsers, IE
+    window.ontouchmove = event.preventDefault; // mobile
+    document.onkeydown = event.preventDefaultForScrollKeys;
 }
 
 function enableScroll() {
     if (window.removeEventListener)
         window.removeEventListener('DOMMouseScroll', event.preventDefault, false);
-    window.onmousewheel = document.onmousewheel = null; 
-    window.onwheel = null; 
-    window.ontouchmove = null;  
-    document.onkeydown = null;  
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
 }
 var lastItem = 0;
+
 function fetchTransactions() {
     disableScroll();
-    if(lastItem > 250){
+    if (lastItem > 250) {
         var node = document.createElement("DIV");
         node.innerHTML = "NO MORE DATA";
         document.getElementById("list").appendChild(node);
-    } else{
+    } else {
 
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "/transactions?id=" + lastItem, true);
-    xmlhttp.send();
-    var xmlDoc = xmlhttp.responseText;
-    xmlhttp.onreadystatechange = function() {
-        console.log(xmlhttp);
-        if (xmlhttp.readyState == 4) {
-            var data = JSON.parse(xmlhttp.responseText);
-        var html = "";
-        console.log('DATA', data);
-        data.forEach(function(item){
-            html += "<p>" + item.name + "</p>";
-            lastItem = item.index + 1;
-        })
-        document.getElementById("list").innerHTML = html;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", "/transactions?id=" + lastItem, true);
+        xmlhttp.send();
+        var xmlDoc = xmlhttp.responseText;
+        xmlhttp.onreadystatechange = function() {
+            console.log(xmlhttp);
+            if (xmlhttp.readyState == 4) {
+                var data = JSON.parse(xmlhttp.responseText);
+                var html = "";
+                console.log('DATA', data);
+                data.forEach(function(item) {
+                    html += "<p>" + item.name + "</p>";
+                    lastItem = item.index + 1;
+                })
+                document.getElementById("list").innerHTML = html;
 
-        }
-        enableScroll();
+            }
+            enableScroll();
 
-    };
+        };
     }
 };
-
-// //Initialize 
-// hideDisplay(send);
-// hideDisplay(transactions);
-// hideDisplay(getElement("errorFormat"));
-// hideDisplay(getElement("errorEmpty"));
